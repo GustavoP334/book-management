904692a3277239816358c1d4caadf49159aeaa39
GustavoP334 <gugupassos1@hotmail.com>
2025-02-07 00:50:55 -0300

    feat: crud completo via API && auth

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    
+    Route::get('/create-writer', [ApiController::class, 'createOrUpdate']);
+
+    Route::put('/update-writer/{id}', [ApiController::class, 'createOrUpdate']);
+
+    Route::get('/delete-writer/{id}', [ApiController::class, 'delete']);
+
+    Route::get('/get-writer', [ApiController::class, 'get']);
+
+    Route::get('/get-writer-books/{id}', [ApiController::class, 'getLivrosAutor']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
